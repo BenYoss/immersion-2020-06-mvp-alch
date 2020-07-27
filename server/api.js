27bc93c4abeca4b56646298f76ca4265e3de2f43
benyoss <benyoss4@gmail.com>
2020-07-27 14:55:37 -0500

    "Created a getAlcohol method that fishes for drink data using API and Axios."

--- server/api.js
+++ server/api.js
@@ -1,7 +1,25 @@
+require('dotenv').config();
 const axios = require('axios');
-
-const getAlcohol = (drinkname) => {
-	
+const { xkey } = process.env;
+const getAlcohol = ({ drinkname }) => {
+	return axios({
+		"method":"GET",
+		"url":"https://the-cocktail-db.p.rapidapi.com/search.php",
+		"headers":{
+		"content-type":"application/octet-stream",
+		"x-rapidapi-host":"the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key":xkey,
+		"useQueryString":true
+		},"params":{
+		"i":`${drinkname}`
+		}
+		})
+		.then((response)=>{
+		  return response.data.ingredients[0];
+		})
+		.catch((error)=>{
+		  console.log(error)
+		})
 };
 
 module.exports = {
